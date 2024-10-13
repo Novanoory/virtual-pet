afbf1afcf8005d83ad30561a9d4c411f5e73a592
Novanoory <noorulainrasheed2082@gmail.com>
2024-10-13 12:40:34 +0500

    create a function to take input from user

---
+++ script.js
@@ -0,0 +1,7 @@
+function createPet() {
+    var name = prompt("What is your pet's name?");
+    var type = prompt("What type of pet do you have? (e.g., dog, cat, dragon, parrot)");
+    var age = parseInt(prompt("How old is your pet? (in years)"), 10);
+    var happiness = parseInt(prompt("How happy is your pet? (0-100)"), 10);
+    var hunger = parseInt(prompt("How hungry is your pet? (0-100)"), 10);
+}
