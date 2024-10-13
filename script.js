d7675f54fed580b56ca779ceffe79a3aa15ac995
Novanoory <noorulainrasheed2082@gmail.com>
2024-10-13 12:45:54 +0500

    three method of feed, play and age are created

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function createPet() {
     var age = parseInt(prompt("How old is your pet? (in years)"), 10);
     var happiness = parseInt(prompt("How happy is your pet? (0-100)"), 10);
     var hunger = parseInt(prompt("How hungry is your pet? (0-100)"), 10);
-}
 
  // Create the pet object
  var pet = {
@@ -13,4 +12,29 @@ function createPet() {
     age: age,
     happiness: happiness,
     hunger: hunger,
- }
+   // Method to feed the pet
+   feed: function() {
+    this.hunger -= 20;
+    if (this.hunger < 0) this.hunger = 0;
+    alert(`${this.name} has been fed! Hunger is now ${this.hunger}.`);
+},
+
+// Method to play with the pet
+play: function() {
+    this.happiness += 20;
+    if (this.happiness > 100) this.happiness = 100;
+    alert(`${this.name} played and is now ${this.happiness} happy!`);
+},
+
+// Method to age the pet
+agePet: function() {
+    this.age += 1;
+    this.happiness -= 5;
+    this.hunger += 10;
+    if (this.happiness < 0) this.happiness = 0;
+    alert(`${this.name} is now ${this.age} years old! Happiness: ${this.happiness}, Hunger: ${this.hunger}`);
+}
+};
+
+return pet;
+}
